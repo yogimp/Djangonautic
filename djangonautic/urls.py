8dc49eb7f9c37fa09a5983ee0774d642325d4cb7
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-03-07 15:02:45 +0700

    creating django app

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.homepage),
+    path('articles/', include('articles.urls')),
     path('about/', views.about),
     path('admin/', admin.site.urls),
 ]
