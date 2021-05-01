64debf882c6240744ad8eb7be9fd6d2d2cd82518
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-05-02 03:43:17 +0700

    URL Parameters

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
     path('', views.article_list),
     path('about/', views.article_list),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail),
 ]
