621d9ce436bf9280b271c5802d4c7a3f7533b0e8
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-05-03 22:20:16 +0700

    Named URL

--- articles/urls.py
+++ articles/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from django.urls import re_path
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name="list"),
     path('about/', views.article_list),
-    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
